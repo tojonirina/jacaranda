1f1a1c1a90fafc121650b9f0e6cd5f677dfad663
tojonirina.randriamalala <tojo.fr007@gmail.com>
2021-02-11 22:51:13 +0300

    Create directories app and models

--- computers/models.py
+++ computers/models.py
@@ -23,28 +23,5 @@ class Computers(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField("date of last update", auto_now=True)
 
-class Directories(models.Model):
-
-    def __str__(self):
-        self.full_name
-
-    class Meta:
-        db_table = 'directories'
-        managed = True
-        verbose_name = 'Directories'
-        verbose_name_plural = 'Directoriess'
-
-    full_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=30)
-    urgent_phone = models.CharField(max_length=30)
-    email = models.EmailField()
-    address = models.CharField(max_length=100)
-    date_of_service = models.DateField()
-    end_of_service = models.DateField(null=True)
-    state = models.IntegerField() # conge|permission|other
-    avatar = models.CharField(max_length=100)
-    note = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 
--- gstock/settings.py
+++ gstock/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # All external app
     'users',
-    'materials'
+    'materials',
+    'computers'
 ]
 
 MIDDLEWARE = [
