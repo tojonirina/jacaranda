c22d617310b9ddae055ddd9612c52f265ab77c18
tojonirina.randriamalala <tojo.fr007@gmail.com>
2021-03-30 21:44:08 +0300

    Edit absence

--- absences/urls.py
+++ absences/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path, include
+from django.urls import path, include
 from .views import AbsenceView
 
 app_name = "absences"
--- absences/views.py
+++ absences/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Absences
 from directories.models import Directories
-import json
 
 class AbsenceView():
 
